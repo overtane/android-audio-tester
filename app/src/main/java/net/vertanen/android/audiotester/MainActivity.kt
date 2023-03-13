6183a15198e831d429a25287ea53199e1ad2a733
Olli Vertanen <olli@vertanen.net>
2023-03-13 02:18:05 +0200

    Initial commit after project creation
    First layout elements to main fragment.

---
+++ app/src/main/java/net/vertanen/android/audiotester/MainActivity.kt
@@ -0,0 +1,18 @@
+package net.vertanen.android.audiotester
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.vertanen.android.audiotester.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
