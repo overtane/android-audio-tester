f87fa289ede4fc105f7fd78be0522f0e8d8042c9
Olli Vertanen <olli@vertanen.net>
2023-03-19 01:29:03 +0200

    Set-up nav-graph and add overflow menu

--- app/src/main/java/com/github/overtane/audiotester/MainActivity.kt
+++ app/src/main/java/com/github/overtane/audiotester/MainActivity.kt
@@ -2,6 +2,9 @@ package com.github.overtane.audiotester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.github.overtane.audiotester.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController.navigateUp()
     }
 }
--- app/src/main/java/com/github/overtane/audiotester/ui/MainFragment.kt
+++ app/src/main/java/com/github/overtane/audiotester/ui/MainFragment.kt
@@ -1,14 +1,16 @@
 package com.github.overtane.audiotester.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import com.github.overtane.audiotester.R
 import com.github.overtane.audiotester.databinding.FragmentMainBinding
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), MenuProvider {
 
     private lateinit var viewModel: MainViewModel
 
@@ -24,7 +26,28 @@ class MainFragment : Fragment() {
         val binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel // bind ui-data to viewModel instance
+
+        (requireActivity() as MenuHost).addMenuProvider(
+            this,
+            viewLifecycleOwner,
+            Lifecycle.State.RESUMED
+        )
+
         return binding.root
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.main_overflow_menu, menu)
+    }
+
+    override fun onMenuItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_about -> {}
+            R.id.menu_devices -> {}
+            R.id.menu_instructions -> {}
+            R.id.menu_recordings -> {}
+        }
+        return false
     }
 
     companion object {
